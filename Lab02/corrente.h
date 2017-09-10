d69d87b1076b419640a39f3d63aa79001a710508
nicolasghirello <nicolas.rosario@hotmail.com>
2017-09-10 02:39:09 -0300

    correção repositório

--- Lab02/corrente.h
+++
@@ -1,9 +0,0 @@
-class corrente {
-	private:
-		agência
-		número
-		saldo
-		status //que informa se ela é especial ou não
-		limite //(no caso de conta especial)
-		conjunto de movimentações //(normalmente em grande número e variável entre contas).
-}	
